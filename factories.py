211a0dd9d014e04d0cd7cb29adb828e986bea046
MrFreuden <mrfreuden@gmail.com>
2026-03-03 18:44:21 +0100

    feat: impl. BikeFactory

--- factories.py
+++ factories.py
@@ -2,40 +2,32 @@
 Factory Pattern: object creation
 """
 
-from models import Bike, Station, Trip, User
+from abc import ABC, abstractmethod
+from models import Bike, ClassicBike, BIKE_TYPES, BIKE_STATUSES, ElectricBike
 
-
-class BikeFactory:
+class BikeFactory(ABC):
     """Factory for creating bike objects"""
     
-    @staticmethod
-    def create_bike():
-        """Create a new bike instance"""
+    @abstractmethod
+    def create_bike(self, id) -> Bike:
         pass
 
-
-class StationFactory:
-    """Factory for creating station objects"""
+class ClassicBikeFactory(BikeFactory):
+    """Factory for creating classic bike objects"""
     
-    @staticmethod
-    def create_station():
-        """Create a new station instance"""
-        pass
+    def create_bike(self, id) -> Bike:
+        return ClassicBike(id=id, status="available", gear_count=21)
 
-
-class TripFactory:
-    """Factory for creating trip objects"""
+class ElectricBikeFactory(BikeFactory):
+    """Factory for creating electric bike objects"""
     
-    @staticmethod
-    def create_trip():
-        """Create a new trip instance"""
-        pass
+    def create_bike(self, id) -> Bike:
+        return ElectricBike(id=id, status="available", battery_level=100, max_range_km=50) 
 
-
-class UserFactory:
-    """Factory for creating user objects"""
+# class UserFactory:
+#     """Factory for creating user objects"""
     
-    @staticmethod
-    def create_user():
-        """Create a new user instance"""
-        pass
+#     @staticmethod
+#     def create_user():
+#         """Create a new user instance"""
+#         pass
